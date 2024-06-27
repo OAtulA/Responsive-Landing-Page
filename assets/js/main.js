01212849a4eae458bfc6e5d51dbf8c51cd0be33b
Atul <oraonatula@gmail.com>
2024-06-27 22:30:26 +0530

    The container x margin set for small device

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,50 +37,7 @@ function scrollHeader() {
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
-
-/*==================== SWIPER DISCOVER ====================*/
-let swiper = new Swiper(".discover__container", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  loop: true,
-  spaceBetween: 32,
-  coverflowEffect: {
-    rotate: 0,
-  },
-});
-
-/*==================== VIDEO ====================*/
-const videoFile = document.getElementById("video-file"),
-  videoButton = document.getElementById("video-button"),
-  videoIcon = document.getElementById("video-icon");
-
-function playPause() {
-  if (videoFile.paused) {
-    // Play video
-    videoFile.play();
-    // We change the icon
-    videoIcon.classList.add("ri-pause-line");
-    videoIcon.classList.remove("ri-play-line");
-  } else {
-    // Pause video
-    videoFile.pause();
-    // We change the icon
-    videoIcon.classList.remove("ri-pause-line");
-    videoIcon.classList.add("ri-play-line");
-  }
-}
-videoButton.addEventListener("click", playPause);
-
-function finalVideo() {
-  // Video ends, icon change
-  videoIcon.classList.remove("ri-pause-line");
-  videoIcon.classList.add("ri-play-line");
-}
-// ended, when the video ends
-videoFile.addEventListener("ended", finalVideo);
-
+ 
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
